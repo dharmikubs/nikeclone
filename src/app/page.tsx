b466064785e36e017b6979733b973ff50539fe85
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-07-31 19:24:35 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,10 +38,10 @@ const HomePage = async () => {
         </div>
         <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
           <h1 className="text-2xl font-bold">New Arrivals</h1>
-          <ProductList
+          {/* <ProductList
             categoryId={process.env.FEATURED_PRODUCT_CATEGORY_ID!}
             limit={3}
-          />
+          /> */}
         </div>
       </div>
     </>
