f6212a38d70b1b83ae3a26c9f75a588618277126
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-07-31 19:22:49 +0530

    update 🚀

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -30,6 +30,7 @@ const ProductList = async ({
           <Link
             href={"/" + product.slug}
             className="mt-12 relative w-full flex flex-col gap-4 sm:w-[45%] lg:w-[25%]"
+            key={product._id}
           >
             <div className="relative w-full h-80 group">
               <Image
