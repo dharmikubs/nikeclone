d6a6c818b4533c7de146dcb6199f4ca4049f4528
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-08-02 18:23:10 +0530

    updated code 🚀

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -26,7 +26,7 @@ const ProductList = async ({
     .find();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
       {res.items.map((product: products.Product) => {
         const sanitizedDescription = DOMPurify.sanitize(
           product.description || ""
